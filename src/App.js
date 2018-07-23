f5bb177335503e9bba957c0a3688101b09a0a10b
learn <learn@learn12.local>
2018-07-23 16:48:35 -0700

    Created components, pages, data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {} from "react-bootstrap"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import List from './components/list'
+import Apartment from './components/apartment'
+import Form from './components/form'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <Router>
+                <div>
+                    <Route path="/list" component={List}/>
+                    <Route path="/apartment/:id" component={Apartment}/>
+                    <Route path="/create" component={Form}/>
+                </div>
+          </Router>
       </div>
     );
   }
---
+++ src/components/list.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {} from "react-bootstrap"
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+
+import apartments from "../data"
+
+class List extends Component {
+  render() {
+    return (
+      <div>
+        {apartments.map((apt) => {
+            console.log(apt.id);
+            return (
+                <div>
+                <Link to={`/apartment/${apt.id}`}>{apt.street}</Link>
+                <p>{apt.city}</p>
+                <p>{apt.zip}</p>
+                <p>{apt.state}</p>
+                <p>{apt.country}</p>
+                </div>
+            )
+        })}
+      </div>
+    );
+  }
+}
+
+export default List;
